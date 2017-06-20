e9da296126abc114721193b16e7388aba9fd637b
teja-rvs <suryateja@greycampus.com>
2017-06-20 11:28:57 +0000

    Update factorial.rb

--- functions/factorial.rb
+++ functions/factorial.rb
@@ -1,16 +1,16 @@
 #!/usr/bin/ruby -w
 
-def fact(n)							# Defining the function fact
+def fact(n)						# Defining the function fact
 	if n == 1 or n == 0
 		return 1
 	else 
-		return n * fact(n-1)		# Recursive call of the function recursion
+		return n * fact(n-1)			# Recursive call of the function recursion
 	end
 end
 
 
 puts "Enter the number" 
 
-n = gets.chomp.to_i     			# Reading the input from console
+n = gets.chomp.to_i     				# Reading the input from console
 
-puts "Factorial is #{fact(n)}"		# Calling the function "fact"
+puts "Factorial is #{fact(n)}"				# Calling the function "fact"

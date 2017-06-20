431c41538ea3a58d722eefcee6385a4a4cfb801a
teja-rvs <suryateja@greycampus.com>
2017-06-20 11:41:08 +0000

    Update file_append.rb

--- file_handling/file_append.rb
+++ file_handling/file_append.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/ruby -w
 
-file1 = File.read("text.txt")				      	# Reading the file 
+file1 = File.read("text.txt")               # Reading the file 
 
-file2 = File.read("text1.txt")				    	# reading the file
+file2 = File.read("text1.txt")              # reading the file
 
-File.write("text2.txt",file1 + file2)		  	# Creating a new file and adding text to it
+File.write("text2.txt",file1 + file2)       # Creating a new file and adding text to it
 
-file3 = File.read('text2.txt')				    	# Storing the contents of file into variable
+file3 = File.read('text2.txt')              # Storing the contents of file into variable
 
-puts 'appended file data:'					    		# Printing the data of text2.txt
+puts 'appended file data:'                  # Printing the data of text2.txt
 puts file3

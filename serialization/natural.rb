2682d910c4b601815bab4dc0383b1057b4ab8599
teja-rvs <suryateja@greycampus.com>
2017-06-23 16:30:02 +0530

    programs

--- serialization/natural.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ruby -w
-
-# reading the input from console
-puts "Enter the n th term"
-# dumpint the data in memory
-x = Marshal.dump(Array.new(gets.chomp.to_i){ |y| y = y+1 })
-puts "serialized number"
-# retrieving the data from memory
-p Marshal.load(x)

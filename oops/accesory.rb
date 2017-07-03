31134922ee2b2b1a7980a9b6511c6f35118d1b67
teja-rvs <suryateja@greycampus.com>
2017-07-03 15:35:08 +0530

    course update

--- oops/accesory.rb
+++ oops/accesory.rb
@@ -37,15 +37,15 @@ class Mobile
   def price
     if (@company.downcase == 'samsung'  or @company.downcase=='moto'or @company.downcase=='lenovo' or @company.downcase=='nokia')
       if(@company.downcase=='samsung')
-                ecost = 7000*(@screensize/4)+7000*(@ram)+7000*@processor+0.05*7000
-              elsif(@company.downcase=='moto')
-                ecost = 5999*(@screensize/4)+5999*(@ram)+5999*@processor+0.05*5999
-              elsif(@company.downcase=='lenovo')
-                ecost = 4000*(@screensize/4)+4000*(@ram)+4000*@processor+0.05*4000
-              else
-                ecost = 6100*(@screensize/4)+6100*(@ram)+6100*@processor+0.05*6100
-            end
-            return ecost
+        ecost = 7000*(@screensize/4)+7000*(@ram)+7000*@processor+0.05*7000
+      elsif(@company.downcase=='moto')
+        ecost = 5999*(@screensize/4)+5999*(@ram)+5999*@processor+0.05*5999
+      elsif(@company.downcase=='lenovo')
+        ecost = 4000*(@screensize/4)+4000*(@ram)+4000*@processor+0.05*4000
+      else
+        ecost = 6100*(@screensize/4)+6100*(@ram)+6100*@processor+0.05*6100
+      end
+      return ecost
     else
       puts "Specified company not available in our catalogue"
     end
--- oops/mclass.rb
+++ oops/mclass.rb
@@ -37,16 +37,16 @@ class Mobile
   def price
     if (@company.downcase == 'samsung'  or @company.downcase=='moto'or @company.downcase=='lenovo' or @company.downcase=='nokia')
       if(@company.downcase=='samsung')
-                ecost = 7000*(@screensize/4)+7000*(@ram)+7000*@processor+0.05*7000
-              elsif(@company.downcase=='moto')
-                ecost = 5999*(@screensize/4)+5999*(@ram)+5999*@processor+0.05*5999
-              elsif(@company.downcase=='lenovo')
-                ecost = 4000*(@screensize/4)+4000*(@ram)+4000*@processor+0.05*4000
-              else
-                ecost = 6100*(@screensize/4)+6100*(@ram)+6100*@processor+0.05*6100
-            end
-              puts "The price of #{@company} mobile is nearly #{ecost.to_i}"
-        else
+        ecost = 7000*(@screensize/4)+7000*(@ram)+7000*@processor+0.05*7000
+      elsif(@company.downcase=='moto')
+        ecost = 5999*(@screensize/4)+5999*(@ram)+5999*@processor+0.05*5999
+      elsif(@company.downcase=='lenovo')
+        ecost = 4000*(@screensize/4)+4000*(@ram)+4000*@processor+0.05*4000
+      else
+        ecost = 6100*(@screensize/4)+6100*(@ram)+6100*@processor+0.05*6100
+      end
+        puts "The price of #{@company} mobile is nearly #{ecost.to_i}"
+    else
       puts "Specified company not available in our catalogue"
     end
   end
